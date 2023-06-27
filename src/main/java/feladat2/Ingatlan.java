e01a7ab2af307072e8dd2ef9daebdb32a16d2e83
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-27 08:18:43 +0200

    create and fill map

--- src/main/java/feladat2/Ingatlan.java
+++ src/main/java/feladat2/Ingatlan.java
@@ -1,5 +1,16 @@
 package feladat2;
 
 public class Ingatlan {
-
+	private String tulajdonos;
+	private int alapterulet;
+	private int becsultErtek;
+	
+	
+	public Ingatlan(String tulajdonos, int alapterulet, int becsultErtek) {
+		this.tulajdonos = tulajdonos;
+		this.alapterulet = alapterulet;
+		this.becsultErtek = becsultErtek;
+	}
+	
+	
 }
