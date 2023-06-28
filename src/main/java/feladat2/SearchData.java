0ef753a3547005f0dca14f4d7b878a3d51660906
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-28 07:23:29 +0200

    Merge branch 'kn_searchby' into kn_bugfix and bugfix

--- src/main/java/feladat2/SearchData.java
+++ src/main/java/feladat2/SearchData.java
@@ -1,13 +1,21 @@
 package feladat2;
 
+import java.util.Map;
 import java.util.Scanner;
 
 public class SearchData {
-	public static String searchByParcelNumber(Scanner sc) {
+	public static String searchByParcelNumber(Scanner sc,Map<String, Ingatlan> ingatlanok) {
 		String parcelNum;
 		System.out.println("Adja meg a helyrajzi számot:");
 		parcelNum = sc.nextLine();
-		return parcelNum;
+		if(ingatlanok.get(parcelNum)!=null) {
+			return parcelNum;
+		}else {
+			System.out.println("Nincs ilyen ingatlan \n\n");
+			return null;
+		}
+		
+		
 
 	}
 }
